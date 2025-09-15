a22af2f6c73ad8d46dd9b4445f676bc4962be29c
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2025-09-15 11:39:22 +0200

    Added Game class

---
+++ Forelesning09/src/Game.java
@@ -0,0 +1,26 @@
+public class Game {
+
+    private String title;
+    private String genre;
+
+    public Game(String title, String genre) {
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
