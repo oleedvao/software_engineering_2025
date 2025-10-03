86ec2736be3b9dc5aaf5dd65a7951eacf05d46b2
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2025-10-03 13:44:42 +0200

    Lecture 15 extra: Created duplicate musicas project

---
+++ Forelesning 14 og 15 - Heksagonal Arkitektur/Ekstra eksempler/musicas/core/src/main/java/org/musicas/core/service/ArtistServiceWithoutDTOs.java
@@ -0,0 +1,45 @@
+package org.musicas.core.service;
+
+import org.musicas.core.domain.Artist;
+import org.musicas.core.domain.Song;
+import org.musicas.core.dto.CreateArtistRequest;
+import org.musicas.core.dto.GetArtistSongsWithLengthRequest;
+import org.musicas.core.dto.GetArtistSongsWithLengthResult;
+import org.musicas.core.dto.SongDTO;
+import org.musicas.core.port.ArtistRepositoryPort;
+
+import java.util.ArrayList;
+
+public class ArtistServiceWithoutDTOs {
+
+    private ArtistRepositoryPort artistRepository;
+
+    public ArtistServiceWithoutDTOs(ArtistRepositoryPort artistRepository) {
+        this.artistRepository = artistRepository;
+    }
+
+    public void createArtist(String name) {
+        Artist artist = new Artist(name);
+        artistRepository.createArtist(artist);
+    }
+
+    public ArrayList<SongDTO> getArtistSongsWithLength(int artistId, int lengthInSeconds) {
+
+        ArrayList<Song> artistSongs = artistRepository.getArtistSongs(artistId);
+
+        ArrayList<SongDTO> artistSongsWithLength = new ArrayList<>();
+
+        for (Song song : artistSongs) {
+            if (song.getLengthInSeconds() >= lengthInSeconds) {
+                SongDTO songDTO = new SongDTO(
+                        song.getId(),
+                        song.getTitle(),
+                        song.getLengthInSeconds()
+                );
+                artistSongsWithLength.add(songDTO);
+            }
+        }
+
+        return artistSongsWithLength;
+    }
+}
